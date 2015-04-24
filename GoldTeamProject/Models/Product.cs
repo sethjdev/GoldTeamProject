91d9fa8e4265eba15bddefac0f37b34ef86bd30a
Seth Johnson <sethjdesigns@gmail.com>
2015-04-24 15:54:31 -0400

    Created product and user classes. Added product properties.

---
+++ GoldTeamProject/Models/Product.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace GoldTeamProject.Models
+{
+    public class Product
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public decimal Price { get; set; }
+        public string Photo { get; set; }
+        public string Description { get; set; }
+        public bool Availability { get; set; }
+        public string Category { get; set; }
+
+        public int UserID { get; set; }
+        public virtual User User { get; set; }
+
+    }
+}
