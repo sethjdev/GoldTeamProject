58da7a80c09930ae0dd81135fce098731160e0f9
Seth Johnson <sethjdesigns@gmail.com>
2015-04-27 16:38:18 -0400

    created main product feed and force json output

--- GoldTeamProject/App_Start/WebApiConfig.cs
+++ GoldTeamProject/App_Start/WebApiConfig.cs
@@ -5,6 +5,7 @@ using System.Net.Http;
 using System.Web.Http;
 using Microsoft.Owin.Security.OAuth;
 using Newtonsoft.Json.Serialization;
+using System.Net.Http.Headers;
 
 namespace GoldTeamProject
 {
@@ -16,7 +17,7 @@ namespace GoldTeamProject
             // Configure Web API to use only bearer token authentication.
             config.SuppressDefaultHostAuthentication();
             config.Filters.Add(new HostAuthenticationFilter(OAuthDefaults.AuthenticationType));
-
+            config.Formatters.JsonFormatter.SupportedMediaTypes.Add(new MediaTypeHeaderValue("text/html"));
             // Web API routes
             config.MapHttpAttributeRoutes();
 
--- GoldTeamProject/Models/Product.cs
+++ GoldTeamProject/Models/Product.cs
@@ -9,14 +9,14 @@ namespace GoldTeamProject.Models
     {
         public int Id { get; set; }
         public string Title { get; set; }
-        public decimal Price { get; set; }
+        public double Price { get; set; }
         public string Photo { get; set; }
         public string Description { get; set; }
         public bool Availability { get; set; }
         public string Category { get; set; }
 
-        public int UserID { get; set; }
-        public virtual User User { get; set; }
+        //public int UserID { get; set; }
+        //public virtual User User { get; set; }
 
     }
 }
