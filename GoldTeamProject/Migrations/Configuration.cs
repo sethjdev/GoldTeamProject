a846c5f6ca47eb3edba115e0b3817b35edb79a8c
Seth Johnson <sethjdesigns@gmail.com>
2015-04-27 16:38:45 -0400

    updated product seed method

--- GoldTeamProject/Migrations/Configuration.cs
+++ GoldTeamProject/Migrations/Configuration.cs
@@ -16,16 +16,14 @@ namespace GoldTeamProject.Migrations
 
         protected override void Seed(GoldTeamProject.Models.GoldTeamProjectContext context)
         {
-            context.Products.AddOrUpdate(
-                p => p.Title,
-                new Product { Title = "Product 1" },
-                new Product { Title = "Product 2" },
-                new Product { Title = "Product 3" },
-                new Product { Title = "Product 4" },
-                new Product { Title = "Product 5" }
+
+            context.Products.AddOrUpdate(x => x.Id,
+                new Product { Id = 1, Title = "Product 1", Price = 19.99, Photo = "http://experimentalart.unsw.wikispaces.net/file/view/product.jpg/361156778/product.jpg", Description = "Product 1 Description", Availability = true, Category = "Category1" },
+                new Product { Id = 2, Title = "Product 2", Price = 12.99, Photo = "http://experimentalart.unsw.wikispaces.net/file/view/product.jpg/361156778/product.jpg", Description = "Product 2 Description", Availability = true, Category = "Category1" },
+                new Product { Id = 3, Title = "Product 3", Price = 13.99, Photo = "http://experimentalart.unsw.wikispaces.net/file/view/product.jpg/361156778/product.jpg", Description = "Product 3 Description", Availability = true, Category = "Category2" },
+                new Product { Id = 4, Title = "Product 4", Price = 16.99, Photo = "http://experimentalart.unsw.wikispaces.net/file/view/product.jpg/361156778/product.jpg", Description = "Product 4 Description", Availability = true, Category = "Category2" },
+                new Product { Id = 5, Title = "Product 5", Price = 17.99, Photo = "http://experimentalart.unsw.wikispaces.net/file/view/product.jpg/361156778/product.jpg", Description = "Product 5 Description", Availability = true, Category = "Category3" }
                 );
-
-
         }
     }
 }
